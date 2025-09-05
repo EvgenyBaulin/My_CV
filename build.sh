3ad48508f77f3de85c9d4fed29c9b06431dda6eb
evgenybaulin <e.baulin@icloud.com>
2025-09-05 04:29:37 +0300

    Update experience of work

--- build.sh
+++ build.sh
@@ -14,14 +14,23 @@ pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Ful
 pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Full_Stack_Developer.tex"
 
 # Собираем Programmer Analyst
-pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Product_Analyst.tex"
+pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Programmer_Analyst.tex"
 pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Programmer_Analyst.tex"
 
+# Собираем Programmer Analyst
+pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Eng.tex"
+pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Eng.tex"
+
+# Собираем Programmer Analyst
+pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Rus.tex"
+pdflatex -interaction=nonstopmode -output-directory=output "CV_Evgeny_Baulin_Rus.tex"
+
 # Переименовываем PDF согласно требованию
-mv "output/CV_Evgeny_Baulin_Full_Stack_Developer.pdf" "output/CV. Evgeny Baulin. Full Stack Developer.pdf"
-mv "output/CV_Evgeny_Baulin_Product_Analyst.pdf" "output/CV. Evgeny Baulin. Product Analyst.pdf"
+mv "output/CV_Evgeny_Baulin_Full_Stack_Developer.pdf" "CV. Evgeny Baulin. Full Stack Developer.pdf"
+mv "output/CV_Evgeny_Baulin_Programmer_Analyst.pdf" "CV. Evgeny Baulin. Programmer Analyst.pdf"
+mv "output/CV_Evgeny_Baulin_Eng.pdf" "CV Evgeny Baulin.pdf"
+mv "output/CV_Evgeny_Baulin_Rus.pdf" "Резюме Евгений Баулин.pdf"
 
-rm output/*.aux output/*.log output/*.out
+rm -rf output
 
-echo "Сборка завершена. Итоговые файлы:"
-ls output
+echo "Сборка завершена"
